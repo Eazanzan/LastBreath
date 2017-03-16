364e304e6dc9c04c8abbad4c1f9c7937d78db815
Eazanzan <ozgunkaya@ku.edu.tr>
2017-03-16 12:18:16 +0300

    Second Part of Initial commit

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+	public GameObject mainMenu;
+	public GameObject playMenu;
+	public GameObject optionsMenu;
+
+	public void mainMenutoPlay(){
+		mainMenu.SetActive (false);
+		playMenu.SetActive (true);
+	}
+
+	public void backToMainMenuPlay(){
+		mainMenu.SetActive (true);
+		playMenu.SetActive (false);
+	}
+
+	public void mainMenuToOptions(){
+		mainMenu.SetActive (false);
+		optionsMenu.SetActive (true);
+	}
+	public void optionsToMainMenu(){
+		mainMenu.SetActive (true);
+		optionsMenu.SetActive (false);
+	}
+
+	public void fonksiyon(bool mainMenuActiveness,bool optionsMenuActiveness){
+		mainMenu.SetActive (mainMenuActiveness);
+		optionsMenu.SetActive (optionsMenuActiveness);
+
+	}
+}
